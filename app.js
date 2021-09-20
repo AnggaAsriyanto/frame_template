c0c9ced4ea3dde6daf4abd5b35f2c848da65f811
AnggaNellow <anggazxcasd1@gmail.com>
2021-09-20 20:55:08 +0700

    change server port

--- app.js
+++ app.js
@@ -9,5 +9,4 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(3000, () => console.log('Server has started on port 3000'));
-// app.listen(process.env.PORT, process.env.IP, () => console.log('Server has started!'));
+app.listen(process.env.PORT, process.env.IP, () => console.log('Server has started!'));
